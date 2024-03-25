21253c39a707cace45e029559b34d7789b667d7c
ogola <ogolajeremy601@gmail.com>
2024-03-25 22:53:58 +0300

    adds-find-lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+// const record = [
+//     { year: "2015", result: "W"},
+//     SDF-FT09AP1HybridPagesMapping Arrays
+//     Home
+//     Modules
+//     Grades4
+//     Assignments
+//     People
+//     Health Check
+//     { year: "2014", result: "N/A"},
+//     { year: "2013", result: "L"},
+// ]
+// let win = record.find(yearWon)
+// console.log()
+function superbowlWin(record){
+    // return record.year("2015").result
+    const win = record.find(game => game.result === "W");
+    
+    return win ? win.year : undefined;
+
+}
